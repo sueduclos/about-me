11e76f9b372037947590ddb27a1cba2d2737c46d
Sue Duclos <sue.duclos@gmail.com>
2019-10-31 17:57:00 -0700

    added question 1

--- js/app.js
+++ js/app.js
@@ -2,8 +2,19 @@
 //console.log('Proof of life');
 
 var userName = prompt('Hello, what is your name?');
-alert('Welcome to my site ' + userName);
+alert('Welcome to my site ' + userName + '!');
+alert('Lets play a guessing game and learn more about me! ');
 
+//Question One
+var questionOne = prompt('Did I attend the University of Oklahoma?').toUpperCase();
+if (questionOne === 'Y' || questionOne === 'YES'); {
+  //console.log('Correct! Boomer Sooner!');
+  alert('Correct! Boomer Sooner!');
+} else if (questionOne === 'N' || questionOne ==='NO'); {
+  alert('Incorrect!');
+}
+
+//Question Two
 
 
 
